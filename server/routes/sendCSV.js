791dfa9b01d4d3a8b8bafa80685da637270d040e
timos <timoscon@gmail.com>
2019-09-10 15:15:22 +0300

    data upload tool v 0.1 - changes for testing 0.1 version

--- server/routes/sendCSV.js
+++ server/routes/sendCSV.js
@@ -1,12 +1,33 @@
 var router = require('express').Router();
+const fs = require('fs');
 
-router.post('/sendCSV', (req, res) => {
-    const csv = req.body.csv;
-    const json = req.body.json;
-    console.log()
+router.post('/', (req, res) => {
+    const csvString = req.body.csv;
+    const jsonString = req.body.json;
+    const file = jsonString.file;
+
+    fs.writeFile('/var/www/html/uploadTool/data/' + 'test.json', jsonString, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('The json was saved!');
+        fs.writeFile('/var/www/html/uploadTool/data/' + 'test.csv', csvString, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('The csv was saved!');
+            fs.writeFile('/var/www/html/uploadTool/data/' + 'test.xlsx', file, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('The file was saved!');
+            });
+        });
+    });
+
     res.send({
         status: 'ok',
-        csv: csv,
+        csv: csvString,
     });
 });
 
