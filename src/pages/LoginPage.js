af25b837b31bc6c76f7707628a964b8a5e5858e6
timos <timoscon@gmail.com>
2019-09-26 11:49:21 +0300

    data upload tool v 0.95 - added Login Screen and logo

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import Box from '@material-ui/core/Box';
 
 import { setCommunity } from '../redux/actions/mainActions';
 import { useAuth0 } from '../components/organisms/Auth0Wrapper';
-import Header from '../components/organisms/Header';
 import { ROUTE_HOME } from '../ROUTES';
+import LogoContentsTemplate from '../components/templates/LogoContentsTemplate';
 
 const LoginPage = () => {
     const dispatch = useDispatch();
-    const { loading, user } = useAuth0();
+    const { loading, user, loginWithRedirect } = useAuth0();
     //user['http://apiKey']
     if (user) {
         const community = user['http://community'];
@@ -18,7 +19,20 @@ const LoginPage = () => {
     } else if (loading) {
         return <center>Loading...</center>;
     } else {
-        return <Header />;
+        return (
+            <LogoContentsTemplate>
+                <center>
+                    <img style={{ width: '180px' }} src={'http://dev.k2p.agroknow.com/img/DataIntegrationToolLogo.png'} />
+                    <Box>
+                        Please{' '}
+                        <a style={{ fontWeight: 'bold', color: '#5996a0', cursor: 'pointer' }} onClick={() => loginWithRedirect({})}>
+                            Log In
+                        </a>{' '}
+                        to enter the tool
+                    </Box>
+                </center>
+            </LogoContentsTemplate>
+        );
     }
 };
 
