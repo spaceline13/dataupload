b4ea66bb96778f48063b5608cb06b091d30fccaa
Timοσ Lanitis <timoscon@gmail.com>
2019-10-25 11:08:48 +0300

    server cors

--- server/server.js
+++ server/server.js
@@ -8,10 +8,21 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 require('dotenv').config();
 
+var allowedOrigins = ['http://localhost:3006', 'https://upload.fodakai.com'];
+
 //CORS
 app.use(
     cors({
-        origin: ['https://upload.foodakai.com','http://localhost:3006'], //the port my react app is running on.
+        origin: function(origin, callback){
+            // allow requests with no origin
+            // (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
         credentials: true,
     }),
 );
