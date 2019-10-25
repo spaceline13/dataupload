80b281ab2cd847f4d2e3e548b52fa75ab72c7982
Timοσ Lanitis <timoscon@gmail.com>
2019-10-25 16:08:55 +0300

    old delimiters

--- server/routes/uploadedData.js
+++ server/routes/uploadedData.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         body: JSON.stringify({
             apikey: apiKey,
             detail: id ? true : false,
-            entityType: 'internal_dataset||internal_stream',
+            entityType: 'internal_datasetORinternal_stream',
             strictQuery: id ? { id } : { dataSource: community },
         }),
     });
